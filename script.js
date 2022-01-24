6945238382de93a66f3f8955d122501d064a7bd3
Erick Alvarez <erick505alvarez@gmail.com>
2022-01-23 17:45:37 -0800

    add responsive media queries

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
 //                 icon.nextElementSibling.classList.add("change");
 //             }
 //             await sleep(4000);
-//         }   
+//         }
 //     }
 // }
 
@@ -30,16 +30,22 @@ const icons = document.querySelectorAll(".section-1-icons i");
 let i = 1;
 
 setInterval(() => {
-    i++;  // increment counter
-    const icon = document.querySelector(".section-1-icons .change");
-    icon.classList.remove("change");
-    // if on last icon, reset change class to first icon
-    if (i > icons.length) {
-        icons[0].classList.add("change");
-        i = 1;
-    } else {
-        icon.nextElementSibling.classList.add("change");
-    };
+  i++; // increment counter
+  const icon = document.querySelector(".section-1-icons .change");
+  icon.classList.remove("change");
+  // if on last icon, reset change class to first icon
+  if (i > icons.length) {
+    icons[0].classList.add("change");
+    i = 1;
+  } else {
+    icon.nextElementSibling.classList.add("change");
+  }
 }, 4000);
 
-
+// navbar animation
+menuIcon = document.querySelector(".menu");
+menuIcon.addEventListener("click", () => {
+  document.querySelectorAll(".target").forEach((item) => {
+    item.classList.toggle("change");
+  });
+});
